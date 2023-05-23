83165fd52281db4dafbbb52a91af66087501b43e
Rade <radisic00@gmail.com>
2023-05-23 19:53:07 +0200

    nikolin 4 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navbar, Nav, Container} from 'react-bootstrap';
+import {Routes, Route, Link} from "react-router-dom";
+import {Minecraft} from './components/Minecraft';
+import {About} from './components/About';
+import {Home} from './components/Home';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="./">Simple Hosting</Navbar.Brand>
+          <Nav className="me-auto">
+            <Link className='nav-link' to="./">Home</Link>
+            <Link className='nav-link' to="./minecraft">Minecraft</Link>
+            <Link className='nav-link' to="./about">About Us</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/minecraft" element={<Minecraft />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
+    </>
+
   );
 }
 
---
+++ src/components/Minecraft.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Minecraft() {
+    return (
+        <>
+        <h1>Minecraft</h1>
+        <p>Soon</p>
+        </>
+    )
+}
